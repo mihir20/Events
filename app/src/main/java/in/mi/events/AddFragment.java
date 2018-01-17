e6306999adfde86cf0cf46a2a5dfe1c5fcde4cf9
mihir20 <mihir20121997@gmail.com>
2018-01-17 19:28:49 +0530

    Image Loading from gallery

--- app/src/main/java/in/mi/events/AddFragment.java
+++ app/src/main/java/in/mi/events/AddFragment.java
@@ -1,14 +1,21 @@
 package in.mi.events;
 
 
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.AppCompatImageView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -16,8 +23,10 @@ import android.view.ViewGroup;
  */
 public class AddFragment extends Fragment {
 
- TextInputEditText titleText, descriptionText;
- AppCompatButton addButton;
+    private static final int RESULT_LOAD_IMAGE = 1;
+    TextInputEditText titleText, descriptionText;
+    AppCompatButton addButton;
+    AppCompatImageView imageView;
 
     public AddFragment() {
         // Required empty public constructor
@@ -25,11 +34,30 @@ public class AddFragment extends Fragment {
 
 
     @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            imageView.setImageURI( selectedImage );
+
+        }
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate( R.layout.fragment_add, container, false );
 
+        imageView = rootView.findViewById( R.id.addImageIV );
+        addButton = rootView.findViewById( R.id.appCompatButtonAddImage );
+        addButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        } );
 
 
 
